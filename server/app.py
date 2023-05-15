0809ed77542e2624e62f744c55ccd606ada8f2ba
Ben Botsford <ben.botsford@flatironschool.com>
2023-05-15 10:33:59 -0500

    update migrations

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Activity, Signup, Camper
+from models import db, Activity, Camper
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
